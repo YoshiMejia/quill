9989d95db5070090578655b0da9914fb32a5aea8
Yoshi <yoshimejia00@gmail.com>
2023-10-29 12:22:44 -0700

    adding in tRPC wrapper to ensure typesafety

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,4 +1,18 @@
-import { router } from './trpc';
-export const appRouter = router({});
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { router, publicProcedure } from './trpc';
+import { TRPCError } from '@trpc/server';
+
+export const appRouter = router({
+  authCallback: publicProcedure.query(() => {
+    const { getUser } = getKindeServerSession();
+    const user = getUser();
+
+    if (!user.id || !user.email) throw new TRPCError({ code: 'UNAUTHORIZED' });
+
+    //check if user is in database
+
+    return { success: true };
+  }),
+});
 
 export type AppRouter = typeof appRouter;
--- src/trpc/trpc.ts
+++ src/trpc/trpc.ts
@@ -3,3 +3,4 @@ const t = initTRPC.create();
 
 export const router = t.router;
 export const publicProcedure = t.procedure;
+//publicProcedure allows us to create an API endpoint that anyone can call
