03093b60567b248e80cd9287892c85fa16c26700
Yoshi <yoshimejia00@gmail.com>
2023-11-02 21:38:50 -0700

    prepping database

---
+++ src/db/index.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from '@prisma/client';
+
+declare global {
+  // eslint-disable-next-line no-var
+  var cachedPrisma: PrismaClient;
+}
+
+let prisma: PrismaClient;
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.cachedPrisma) {
+    global.cachedPrisma = new PrismaClient();
+  }
+  prisma = global.cachedPrisma;
+}
+
+export const db = prisma;
+
+//this page makes sure we only have a single cached instance of prisma in our application
